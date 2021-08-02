eae8ee1e519d644fc8e86c1191b174859ce40d30
zmnbnm <cml_99@naver.com>
2021-08-02 14:12:48 +0000

    첫업로드_

---
+++ js/data.js
@@ -0,0 +1,69 @@
+const qnaList = [
+    {
+        q:'지옥철 뚫고 회사 도착! 시계를 보니?',
+        a:[
+            {answer:'출근 시간 10분 전 도착.\n 지각 그게 뭐죠?',type:['INTP','ISFJ','ISTJ','INFP']},
+            {answer:'8시 59분 도착. \n 매일매일 짜릿한 출근.',type:['ENTJ','ESFJ','ESTP','ENFP']}
+        ]
+
+    },
+    {
+        q:'2.친구고민들어줄때공감잘함',
+        a:[
+            {answer:'안이오',type:['ENTJ','ESTP','INTP','ISTJ']},
+            {answer:'네',type:['ESFJ','ENFP','ISFJ','INFP']}
+        ]
+
+    },
+    {
+        q:'3.계획적임',
+        a:[
+            {answer:'네',type:['ENTJ','ESFJ','ISFJ','ISTJ']},
+            {answer:'아니오',type:['ESTP','ENFP','INTP','INFP']}
+        ]
+
+    },
+    {
+        q:'4.집에있는게행복함',
+        a:[
+            {answer:'아니오',type:['ENTJ','ESFJ','ESTP','ENFP']},
+            {answer:'네',type:['INTP','ISFJ','ISTJ','INFP']}
+        ]
+
+    }
+]
+const infoList = [
+    
+    {
+        mbti:'ENTJ',
+        desc:'인트제다'
+    },
+    {
+        mbti:'ESFJ',
+        desc:'엣프제다 '
+    },
+    {
+        mbti:'ESTP',
+        desc:'엣팁이다 '
+    },    
+    {
+        mbti:'ENFP',
+        desc:'엔프피다 '
+    },
+    {
+        mbti:'INTP',
+        desc:'인팁이다 '
+    },
+    {
+        mbti:'ISFJ',
+        desc:'잇프제다 '
+    },
+    {
+        mbti:'ISTJ',
+        desc:'잇트제다 '
+    },
+    {
+        mbti:'INFP',
+        desc:'인프피다'
+    }    
+]
---
+++ js/start.js
@@ -0,0 +1,203 @@
+const main = document.querySelector("#main");
+const qna = document.querySelector("#qna");
+const loading = document.querySelector("#loading");
+const result = document.querySelector("#result");
+
+
+const qnaBG = document.querySelector('.QnA-BG');
+const qnaBox = document.querySelector('.QnABox-BG');
+const endPoint = qnaList.length;
+const select = [];
+
+
+function begin()
+{
+    main.style.WebkitAnimation = "MidUp 0.6s";
+    main.style.animation ="MidUp 0.6s";
+    
+    setTimeout(()=>{
+        
+        setTimeout(()=>{
+                main.style.display = "none";
+                qna.style.display = "block";
+        },270)
+        qnaBG.style.WebkitAnimation = "TopDown 0.6s";
+        qnaBG.style.animation ="TopDown 0.6s";
+        qnaBox.style.WebkitAnimation = "DownUp 0.6s";
+        qnaBox.style.animation ="DownUp 0.6s";
+        
+        let qIdx = 0;
+        goNext(qIdx);
+    },270)
+}
+
+
+
+
+function calResult(){
+    var pointArray = [
+        { mbti: 'ENTJ', value:0,key:0},
+        { mbti: 'ESFJ', value:0,key:1},
+        { mbti: 'ESTP', value:0,key:2},
+        { mbti: 'ENFP', value:0,key:3},
+        { mbti: 'INTP', value:0,key:4},
+        { mbti: 'ISFJ', value:0,key:5},
+        { mbti: 'ISTJ', value:0,key:6},
+        { mbti: 'INFP', value:0,key:7},
+    ]
+    
+    for(let i = 0; i<endPoint; i++){
+        var target = qnaList[i].a[select[i]];
+        for(let j=0; j<target.type.length; j++){
+            for(let k =0; k<pointArray.length; k++){
+                if(target.type[j] === pointArray[k].mbti){
+                    pointArray[k].value+=1;
+                }
+            }
+        }
+    }
+    
+    var resultArray = pointArray.sort(function (a,b){
+       if(a.value>b.value){
+        return-1;
+    }                 
+        if(a.value<b.value){
+            return 1;
+        }
+        return 0;
+    });
+    
+    let resultword = resultArray[0].key;
+    return resultword;
+    
+}
+
+function addAnswer(answerText,qIdx,idx)
+{
+    var a = document.querySelector('.A-Box');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+        answer.classList.add('mx-auto');
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+    
+    answer.addEventListener("click", function(){
+        nextAni();
+        var children = document.querySelectorAll('.answerList');
+        for(let i = 0; i < children.length; i++){
+            children[i].disabled = true;
+            select[qIdx] = idx;
+        }
+        
+        var q = document.querySelector('.QnABox-BG2')
+        q.appendChild(answer);
+        answer.className="Q-Box-Answered";
+        
+        setTimeout(()=>{
+         for(let i = 0; i < children.length; i++){
+         children[i].style.display= 'none';
+        }
+        goNext(++qIdx);
+        },500);
+
+    },false);
+}
+
+
+function setResult(){
+    
+    let point = calResult();
+    location.href = './result'+0+'.html';
+    
+    return;
+    
+    const resulname = document.querySelector('.resultname');
+    resulname.innerHTML = infoList[point].mbti;
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = './image/ham'+point+'.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+    
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+    
+}
+
+function goResult(){
+    
+    loading.style.WebkitAnimation = "fadeOut 1s";
+    loading.style.animation ="fadeOut 1s";
+    
+    setTimeout(()=>{
+        result.style.animation ="fadeIn 1s";
+        result.style.WebkitAnimation = "fadeIn 1s";
+
+    },1000)
+
+    setResult();
+}
+
+function goNext(qIdx){
+    
+    if(qIdx === endPoint){
+        resultLoading();
+        return;
+    }
+
+    
+    var q = document.querySelector('.Q-Box')
+    q.innerHTML = qnaList[qIdx].q;
+    var q_img = document.querySelector('.QnA-img');
+    q_img.src='./image/ham'+qIdx+'.png';
+    
+    for(let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer,qIdx,i);
+    }
+    
+    var status = document.querySelector('.statusBar');
+    status.style.width = (100/endPoint) * (qIdx+1) +'%';
+}
+
+
+function nextAni()
+{
+
+    qnaBG.style.WebkitAnimation = "MidUp 1s";
+    qnaBG.style.animation ="MidUp 1s";
+    
+    setTimeout(()=>{
+
+        setTimeout(()=>{
+        },490)
+                
+        qnaBG.style.WebkitAnimation = "TopDown 1s";
+        qnaBG.style.animation = "TopDown 1s";
+    },490)
+}
+
+
+
+function resultLoading(){
+    qnaBG.style.WebkitAnimation = "MidUp 1s";
+    qnaBG.style.animation ="MidUp 1s";
+    qnaBox.style.WebkitAnimation = "MidDown 1s";
+    qnaBox.style.animation ="MidDown 1s";
+    
+        setTimeout(()=>{
+        loading.style.animation ="fadeIn 1s";
+        loading.style.WebkitAnimation = "fadeIn 1s";
+        
+        setTimeout(()=>{
+                qna.style.display = "none";
+                loading.style.display = "block";
+        },490)
+
+    },490)
+
+   setTimeout(goResult,4000);
+}
+
+
